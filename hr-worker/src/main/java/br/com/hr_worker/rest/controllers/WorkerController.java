dd62e9934b42c713ee7e42e6abd8985e6f2de748
Paulo-vitorCS <paulo.vitorcs@hotmail.com>
2024-09-17 20:43:21 -0300

    Zuul timeout

--- hr-worker/src/main/java/br/com/hr_worker/rest/controllers/WorkerController.java
+++ hr-worker/src/main/java/br/com/hr_worker/rest/controllers/WorkerController.java
@@ -37,6 +37,13 @@ public class WorkerController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         logger.info("PORT = " + env.getProperty("local.server.port"));
 
         Worker worker = workerService.findById(id);
